e5d8a406ec013832b44483636ea11934b10d2fa7
cesar <cesar57927@gmail.com>
2020-02-10 21:09:54 -0500

    comment allow blank

--- django_comments/abstracts.py
+++ django_comments/abstracts.py
@@ -57,7 +57,7 @@ class CommentAbstractModel(BaseCommentAbstractModel):
     user_email = models.EmailField(_("user's email address"), blank=True)
     user_url = models.URLField(_("user's URL"), blank=True)
 
-    comment = models.TextField(_('comment'), max_length=COMMENT_MAX_LENGTH)
+    comment = models.TextField(_('comment'), max_length=COMMENT_MAX_LENGTH, blank=True)
 
     # Metadata about the comment
     submit_date = models.DateTimeField(_('date/time submitted'), default=None, db_index=True)
